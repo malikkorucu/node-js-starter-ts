0bd3ec173232da97f966ea475823c834a3e5d9bd
malikkorucu <58563002+malikkorucu@users.noreply.github.com>
2021-08-18 00:51:59 +0300

    mongoose paketi entegre edildi ve db bağlantısı yapıldı

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,38 +1,44 @@
-import { Controller, Param, Body, Get, Req, QueryParam, Res, Post, Put, Delete } from 'routing-controllers';
+import {
+  Controller,
+  // Param,
+  // Body,
+  // Get,
+  // Req,
+  // QueryParam,
+  // Res,
+  // Post,
+  // Put,
+  // Delete,
+} from 'routing-controllers';
 
 @Controller()
 export class UserController {
-  @Get('/users/:id')
-  public getAll(@Param("id") id: number, @Req() request: any, @Res() response: any) {
-    console.log(id)
-    return response.send('Hello response!');
-  }
-
-  @Get("/users")
-  public getUsers(@QueryParam("limit") limit: number, @Req() request: any, @Res() response: any) {
-    console.log(limit)
-    return response.status(200).json({
-      malik:'aslşdfkjasdf'
-    })
-  }
-
-  @Get('/users/:id')
-  public getOne(@Param('id') id: number) {
-    return 'This action returns user #' + id;
-  }
-
-  @Post('/users')
-  public post(@Body() user: any) {
-    return 'Saving user...';
-  }
-
-  @Put('/users/:id')
-  public put(@Param('id') id: number, @Body() user: any) {
-    return 'Updating a user...';
-  }
-
-  @Delete('/users/:id')
-  public remove(@Param('id') id: number) {
-    return 'Removing user...';
-  }
+  // @Get('/users/:id')
+  // public getAll(@Param("id") id: number, @Req() request: any, @Res() response: any) {
+  //   console.log(id)
+  //   return response.send('Hello response!');
+  // }
+  // @Get("/users")
+  // public getUsers(@QueryParam("limit") limit: number, @Req() request: any, @Res() response: any) {
+  //   console.log(limit)
+  //   return response.status(200).json({
+  //     malik:'aslşdfkjasdf'
+  //   })
+  // }
+  // @Get('/users/:id')
+  // public getOne(@Param('id') id: number) {
+  //   return 'This action returns user #' + id;
+  // }
+  // @Post('/users')
+  // public post(@Body() user: any) {
+  //   return 'Saving user...';
+  // }
+  // @Put('/users/:id')
+  // public put(@Param('id') id: number, @Body() user: any) {
+  //   return 'Updating a user...';
+  // }
+  // @Delete('/users/:id')
+  // public remove(@Param('id') id: number) {
+  //   return 'Removing user...';
+  // }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import "reflect-metadata";
-import express from 'express'
+import 'reflect-metadata';
+import express from 'express';
 import { createExpressServer } from 'routing-controllers';
 import { UserController } from './controllers/UserController';
+import connectDatabase from './helpers/db'
+import dotenv from 'dotenv'
 
+dotenv.config();
+
+ 
 const app = createExpressServer({
   routePrefix: '/api',
-  controllers: [UserController], 
+  controllers: [UserController],
 });
+
+connectDatabase()
 
 app.use(express.json());
 
-app.listen(3000 , ()=> {
-  console.log('server started')
+app.listen(3000, () => {
+  console.log('server started');
 });
